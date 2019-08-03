9799d6655579fd872b61211ec78a9ea2b0ce51d4
454447236@qq.com <454447236@qq.com>
2019-08-03 16:40:31 +0800

    改为模板中间件

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
-const Koa=require('koa')
-const app=new Koa()
-const {htmlTpl,ejsTpl,pugTpl} =require('./tpl')
-//const ejs=require('ejs')
-const pug=require('pug')
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const { resolve } = require('path')
 
-app.use(async(ctx,next)=>{
-    ctx.type='text/html; charset=utf-8'
-    ctx.body=pug.render(pugTpl,{you:'YBF',me:'pug'})
+app.use(views(resolve(__dirname, './views'), { extension: 'pug' }))
+
+app.use(async (ctx, next) => {
+    await ctx.render('index', {
+        you: "YBF",
+        me: "Koa"
+    })
 })
 app.listen(8080)
